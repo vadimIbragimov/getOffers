f304edc97a8d55f97ec80b34fd7eb9b8a818b4e8
Petrov012110 <grisha14.06.96@gmail.com>
2021-03-17 23:54:41 +0500

    animation

--- src/components/pages/HomePage/BrandTooltip.tsx
+++ src/components/pages/HomePage/BrandTooltip.tsx
@@ -3,12 +3,14 @@ import { EnterOutlined } from '@ant-design/icons';
 import './style/BrandTooltip.less';
 
 export const  BrandTooltip = () => (
-  <div className="brand-tooltip">
-    <EnterOutlined rotate={90} className="brand-tooltip-icon" />
+  <div className="brand-tooltip ">
+    {/* <EnterOutlined rotate={90} className="brand-tooltip-icon" /> */}
+    <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css"></link>
+    <span className="lnr lnr-arrow-up animate__animated animate__bounceInLeft"></span> 
     <article className="brand-tooltip-text">
-      Нажмите на кнопку
+      Выберите бренд
       <br />
-      чтобы выбрать бренд
+      или модель
     </article>
   </div>
 );
