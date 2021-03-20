c89c333c913bb1c2fd4a3f487a02d8795e3eaf18
Petrov012110 <grisha14.06.96@gmail.com>
2021-03-20 16:24:43 +0500

    added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Menu, Layout} from 'antd';
 import '../styles/Header.less';
 import { Link } from 'react-router-dom';
-
+import logo from '../assets/images/Logo.png'
 export const Header = () => (
 	<Layout.Header className="app-header">
+		
 		<h1>
-			My app
+			<a href="#">
+				<img className="logo-header" src={process.env.PUBLIC_URL + '../assets/images/Logo.png:'} alt="logo"/>
+			</a>
 		</h1>
 		
 	</Layout.Header>
