077bf90885a44e9212b463eab562e7d8c4606605
ibragimov.v.r <ibragimov.v.r@yandex.ru>
2021-03-20 21:25:54 +0500

    Изменён адрес сервера

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const serverAddress = 'http://localhost:808/';
+export const serverAddress = 'http://212.119.243.127:5387/';
 export const BREAKPOINTS = { mobile: 0, tablet: 768, desktop: 1280 };
