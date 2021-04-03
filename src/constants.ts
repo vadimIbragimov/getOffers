bdbeee539aeeb561091028bd310c174541360867
ibragimov.v.r <ibragimov.v.r@yandex.ru>
2021-04-04 00:31:07 +0500

    Вернул адрес сервера

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const serverAddress = 'http://localhost:808/'; //'http://212.119.243.127:5387/';
+export const serverAddress = 'http://212.119.243.127:5387/';//'http://localhost:808/'; //
 export const BREAKPOINTS = { mobile: 0, tablet: 768, desktop: 1280 };
