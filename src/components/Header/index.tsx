3c73c2c8d65fde6fd3bc909ddceeaef12dda5862
ibragimov.v.r <ibragimov.v.r@yandex.ru>
2021-04-03 15:37:38 +0500

    Добавил ссылку на группу

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Menu, Layout } from 'antd';
 import './index.less';
 import { Link } from 'react-router-dom';
+import Icon from '@ant-design/icons';
+import { ReactComponent as vkLogo } from './vk-1.svg';
 
 const logo = require('./Logo.png');
-console.log(logo);
 
 export const Header = () => (
 	<Layout.Header className="app-header">
@@ -12,6 +13,11 @@ export const Header = () => (
 			<Link className="home-link" to="/">
 				<img className="logo-header" src={logo?.default} alt="logo" />
 			</Link>
+		</article>
+		<article className="right-panel">
+			<a href="https://vk.com/get.offers/" target="_blank" rel="noreferrer" className="vk-group-link">
+				<Icon component={vkLogo} style={{ fontSize: '30px' }} />
+			</a>
 		</article>
 	</Layout.Header>
 );
